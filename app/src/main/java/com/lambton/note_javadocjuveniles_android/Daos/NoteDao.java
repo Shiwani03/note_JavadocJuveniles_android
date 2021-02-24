5cbc62c1614eeb381daf7d2038eb3a3c5c7612ef
kiranabamrahjandu <kirna1851994@gmail.com>
2021-02-24 08:14:00 -0500

    Created Model of notes and subject

---
+++ app/src/main/java/com/lambton/note_javadocjuveniles_android/Daos/NoteDao.java
@@ -0,0 +1,46 @@
+package com.lambton.note_javadocjuveniles_android.Daos;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.javadocjuveniles.Models.Notes;
+
+import java.util.List;
+
+@Dao
+public interface NoteDao {
+    @Query("SELECT * FROM Notes ")
+    List<Notes> getAll();
+
+    /*
+     * Insert the object in database
+     * @param note, object to be inserted
+     */
+    @Insert
+    void insert(Notes note);
+
+    /*
+     * update the object in database
+     * @param note, object to be updated
+     */
+    @Update
+    void update(Notes repos);
+
+    /*
+     * delete the object from database
+     * @param note, object to be deleted
+     */
+    @Delete
+    void delete(Notes note);
+
+    /*
+     * delete list of objects from database
+     * @param note, array of objects to be deleted
+     */
+    @Delete
+    void delete(Notes... note);      // Note... is varargs, here note is an array
+
+}
---
+++ app/src/main/java/com/lambton/note_javadocjuveniles_android/Daos/SubjectDao.java
@@ -0,0 +1,53 @@
+package com.lambton.note_javadocjuveniles_android.Daos;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+import com.lambton.note_javadocjuveniles_android.Models.Subjects;
+import com.lambton.note_javadocjuveniles_android.Models.SubjectsWithNotes;
+
+import java.util.List;
+
+@Dao
+public interface SubjectDao {
+    @Query("SELECT * FROM Subjects ")
+     List<SubjectsWithNotes> loadSubjectsWithNotes();
+
+    @Query("SELECT * FROM Subjects ")
+    List<Subjects> getAll();
+
+    @Query("SELECT * FROM Subjects WHERE subject_id = :id")
+    List<Subjects> getSubject(int id);
+
+
+    /*
+     * Insert the object in database
+     * @param note, object to be inserted
+     */
+    @Insert
+    void insert(Subjects subject);
+
+    /*
+     * update the object in database
+     * @param note, object to be updated
+     */
+    @Update
+    void update(Subjects repos);
+
+    /*
+     * delete the object from database
+     * @param note, object to be deleted
+     */
+    @Delete
+    void delete(Subjects subject);
+
+    /*
+     * delete list of objects from database
+     * @param note, array of objects to be deleted
+     */
+    @Delete
+    void delete(Subjects... subject);      // Note... is varargs, here note is an array
+
+}
